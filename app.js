8e9983013e1699402adf22d47fb6118f61116a5a
Victor Garcia <victorgarciaparis13@gmail.com>
2017-12-17 21:25:26 +0100

    avancement

--- app.js
+++ app.js
@@ -1,27 +1,55 @@
+require('dotenv-extended').load();
+
 const RESTIFY = require("restify");
-const BOTBUILDER = require("botbuilder");
-const SERVER = RESTIFY.createServer();
+const builder = require("botbuilder");
+const server = RESTIFY.createServer();
 const PORT = process.env.PORT || 3887;
 
+const SearchTime = require('./dialogs/SearchTime.js');
 
+let LUIS_MODEL_URL = "https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/2b52acd0-ddd1-4a8e-945a-bbcc4dd72123?subscription-key=abe662e52d9e4347a55023acb7f33cb4&verbose=true&timezoneOffset=0&q="
 
-SERVER.listen(PORT, () => {
-  console.log(`${SERVER.name} bot started on port ${PORT}`);
+server.listen(PORT, () => {
+  console.log(`${server.name} bot started on port ${PORT}. Url: ${server.url}`);
 });
-
-const CONNECTOR = new BOTBUILDER.ChatConnector({
+const connector = new builder.ChatConnector({
   appId: process.env.MICROSOFT_APP_ID,
   appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
 
-SERVER.post("/api/messages", CONNECTOR.listen());
+server.post("/api/messages", connector.listen());
 
-var bot = new BOTBUILDER.UniversalBot(CONNECTOR, [
-  function(session) {
-    return session.beginDialog('parcours:home');
-  },
-]);
+var bot = new builder.UniversalBot(connector, [(session) => {
+    session.send(`Je n'ai pas compris ce que vous avez dit`);
+}]);
 
-const parcours = require('./dialogs/parcours.js')
+var recognizer = new builder.LuisRecognizer(process.env.LUIS_MODEL_URL);
+bot.recognizer(recognizer);
 
-bot.library(parcours);
+bot.dialog('SearchTime', [
+  (session, args, next) => {
+    if (!args.loop) {
+      session.dialogData.searchParams.originEntity = builder.EntityRecognizer.findEntity(args.intent.entities, 'Origin');
+      session.dialogData.searchParams.destinationEntity = builder.EntityRecognizer.findEntity(args.intent.entities, 'Destination');
+      session.dialogData.searchParams.transportModeEntity = builder.EntityRecognizer.findEntity(args.intent.entities, 'TransportMode');   
+    }
+    
+    if (!!session.dialogData.searchParams.originEntity) {
+      builder.Prompts.text(` D'où voulez vous partir ? `);
+      session.replaceDialog('SearchTime', {loop: true});
+    }
+
+    if (!!session.dialogData.searchParams.destinationEntity) {
+      builder.Prompts.text(` Où voulez vous aller ? `);
+      session.replaceDialog('SearchTime', {loop: true});
+    }
+
+    if (!!session.dialogData.searchParams.transportModeEntity) {
+      builder.Prompts.text(` Comment souhaitez vous y rendre? `);
+      session.replaceDialog('SearchTime', {loop: true});
+    }
+  }, 
+  (session, results, next) => {
+
+  }
+])
