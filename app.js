593bc7287a3cb0ca240fc21ac18281e9186b05ec
Victor Garcia <victorgarciaparis13@gmail.com>
2017-12-18 14:52:00 +0100

    LUIS added

--- app.js
+++ app.js
@@ -7,10 +7,8 @@ const PORT = process.env.PORT || 3887;
 
 const SearchTime = require('./dialogs/SearchTime.js');
 
-let LUIS_MODEL_URL = "https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/2b52acd0-ddd1-4a8e-945a-bbcc4dd72123?subscription-key=abe662e52d9e4347a55023acb7f33cb4&verbose=true&timezoneOffset=0&q="
-
 server.listen(PORT, () => {
-  console.log(`${server.name} bot started on port ${PORT}. Url: ${server.url}`);
+  console.log(`${server.name} bot started on port ${PORT}`);
 });
 const connector = new builder.ChatConnector({
   appId: process.env.MICROSOFT_APP_ID,
@@ -29,27 +27,34 @@ bot.recognizer(recognizer);
 bot.dialog('SearchTime', [
   (session, args, next) => {
     if (!args.loop) {
+      console.log(args.intent.entities);
       session.dialogData.searchParams.originEntity = builder.EntityRecognizer.findEntity(args.intent.entities, 'Origin');
       session.dialogData.searchParams.destinationEntity = builder.EntityRecognizer.findEntity(args.intent.entities, 'Destination');
-      session.dialogData.searchParams.transportModeEntity = builder.EntityRecognizer.findEntity(args.intent.entities, 'TransportMode');   
+      session.dialogData.searchParams.transportModeEntity = builder.EntityRecognizer.findEntity(args.intent.entities, 'TransportModeV2');  
     }
     
     if (!!session.dialogData.searchParams.originEntity) {
-      builder.Prompts.text(` D'où voulez vous partir ? `);
-      session.replaceDialog('SearchTime', {loop: true});
+      builder.Prompts.text(session, ` D'où voulez vous partir ? `);
     }
 
     if (!!session.dialogData.searchParams.destinationEntity) {
-      builder.Prompts.text(` Où voulez vous aller ? `);
-      session.replaceDialog('SearchTime', {loop: true});
+      builder.Prompts.text(session, ` Où voulez vous aller ? `);
     }
 
     if (!!session.dialogData.searchParams.transportModeEntity) {
-      builder.Prompts.text(` Comment souhaitez vous y rendre? `);
-      session.replaceDialog('SearchTime', {loop: true});
+      builder.Prompts.text(session, ` Comment souhaitez vous y rendre? `);
     }
   }, 
   (session, results, next) => {
-
+    
   }
-])
+]).triggerAction({
+  matches: "SearchTime"
+});
+
+
+bot.dialog('Help', function (session) {
+  session.endDialog(`Boujour! Essayez de me poser une question commme: Temps pour aller de Paris a Marseille en vélo `);
+}).triggerAction({
+  matches: 'Help'
+});
