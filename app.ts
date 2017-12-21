0db3009aad567804d78f8b34dc38f38092a7c7f0
Victor Garcia <victorgarciaparis13@gmail.com>
2017-12-21 16:37:23 +0100

    ameliorations

--- app.ts
+++ app.ts
@@ -2,8 +2,10 @@
 require('dotenv-extended').load();
 import * as RESTIFY from "restify";
 import * as builder from "botbuilder";
+import * as globalTunnel from "global-tunnel";
 const server = RESTIFY.createServer();
 const PORT = process.env.PORT || 3887;
+
 
 import { SearchTime } from './dialogs/SearchTime';
 
@@ -25,19 +27,9 @@ var bot = new builder.UniversalBot(connector, [(session) => {
 var recognizer = new builder.LuisRecognizer(process.env.LUIS_MODEL_URL);
 bot.recognizer(recognizer);
 
-// Logic
-
-const transport_mode = ['driving', 'bicycling', 'walking', 'transit'];
-const transport_mode_verbose = ['Voiture', 'Vélo', 'À pied', "Transports en commun"];
-
-
-
 // Dialogs
 
 bot.library(SearchTime);
-
-
-
 
 bot.dialog('Help', function (session) {
   session.endDialog(`Boujour! Essayez de me poser une question commme: Temps pour aller de Paris a Marseille en vélo `);
